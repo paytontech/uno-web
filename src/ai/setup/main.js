46f797140421754f5cb96c1a87109982171745dc
paytondev <81662879+paytontech@users.noreply.github.com>
2022-05-23 13:36:46 -0400

    somewhat working sort of but also not

--- src/ai/setup/main.js
+++ src/ai/setup/main.js
@@ -1,13 +1,19 @@
 var settings = {
-    aiPlayers = 4,
-    gameMode = "normal"
+    aiPlayers: "4",
+    gameMode: "normal"
 }
-localStorage.setItem("aisettings", settings.json())
+localStorage.setItem("aisettings", JSON.stringify(settings));
 var settingsStore = localStorage.getItem("aisettings")
 var slider = document.getElementById("playerCount");
 var playerText = document.getElementById("sliderAmount")
-playerText.innerHTML = slider.value
-slider.oninput = function() {
-    output.innerHTML = slider.value;
+function onLaunch() {
+    slider.value = String(JSON.parse(localStorage.getItem("aisettings")).aiPlayers);
+    console.log(JSON.parse(localStorage.getItem("aisettings")).aiPlayers)
+    playerText.innerHTML = slider.value + " players";
+    settings = JSON.parse(localStorage.getItem("aisettings"));
 }
-
+function updateSlider(val) {
+    playerText.innerHTML = val + " players";
+    settings.aiPlayers = val;
+    localStorage.setItem("aisettings", JSON.stringify(settings));
+}
