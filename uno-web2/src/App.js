e329832f94a03dc63f830747c59c9ca66e107a48
paytondev <81662879+paytontech@users.noreply.github.com>
2022-11-21 13:19:13 -0500

    zero clue whats going on - will fix later ig

--- uno-web2/src/App.js
+++ uno-web2/src/App.js
@@ -8,10 +8,14 @@ var cards = {
   special: []
 }
 
-
-function App() {
-  const [gameData, setGameData] = useState({
-    players: [],
+var gameData = {
+    players: [
+        {
+            bot: false,
+            id: 0,
+            cards: []
+        }
+    ],
     stack: {
         current: {
             color: "",
@@ -29,76 +33,81 @@ function App() {
         bot: false,
         id: 0
     }
-})
+}
+
+
+function App() {
   useEffect(() => {
     createCards()
-  })
+    createPlayers(4)
+  }, [])
   return (
     <div className="App">
-      
+      <p>{gameData.players[0].cards}</p>
+      <button onClick={distCards}>dist test</button>
     </div>
   );
 }
 
 
-function createCards() {
-  //red
-  for (let i = 0; i < 10; i++) {
-      cards.red.push({
-          color: "red",
-          number: i
-      })
-      console.log(cards)
-  }
-  //green
-  for (let i = 0; i < 10; i++) {
-      cards.green.push({
-          color: "green",
-          number: i
-      })
-  }
-  //blue
-  for (let i = 0;i < 10; i++) {
-      cards.blue.push({
-          color: "blue",
-          number: i
-      })
-  }
-  //yellow
-  for (let i = 0; i < 10; i++) {
-      cards.yellow.push({
-          color: "yellow",
-          number: i
-      })
-  }
-  
+function createPlayers(botCount) {
+    for (let i = 0; i < botCount; i++) {
+        gameData.players.push({
+            bot: true,
+            id: i,
+            cards: []
+        })
+    }
+    console.log(gameData.players)
 }
 
 function distCards() {
-  
+    for (let i = 1; i < gameData.players.length; i++) {
+        for (let z = 0; z < 7; i++) {
+            gameData.players[i].cards.push(genCard())
+        }
+    }
 }
 
 function genCard() {
   //generate two random variables: a random number to determine color of card
-  var cardColorRand = Math.floor(Math.random() * 4)
+  var possibleColors = ["green", "red", "blue", "yellow"]
+  var cardColor = possibleColors[Math.floor(Math.random()*(possibleColors.length - 1))]
   //and another random number to determine the number of the card
   var cardNumberRand = Math.floor(Math.random() * 10)
-  //boring programming stuff
-  switch (cardColorRand) {
-      case 0:
-          return {"color":"red",number:cardNumberRand}
-          break
-      case 1:
-          return {"color":"green",number:cardNumberRand}
-          break
-      case 2:
-          return {"color":"yellow",number:cardNumberRand}
-          break
-      case 3:
-          return {"color":"blue",number:cardNumberRand}
-          break
-      default:
-        break
-  }
+  return {"color":cardColor,number:cardNumberRand}
+  
 }
+function createCards() {
+    //red
+    for (let i = 0; i < 10; i++) {
+        cards.red.push({
+            color: "red",
+            number: i
+        })
+        console.log(cards)
+    }
+    //green
+    for (let i = 0; i < 10; i++) {
+        cards.green.push({
+            color: "green",
+            number: i
+        })
+    }
+    //blue
+    for (let i = 0;i < 10; i++) {
+        cards.blue.push({
+            color: "blue",
+            number: i
+        })
+    }
+    //yellow
+    for (let i = 0; i < 10; i++) {
+        cards.yellow.push({
+            color: "yellow",
+            number: i
+        })
+    }
+    
+  }
 export default App;
